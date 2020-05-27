feea605932ab944d3bd1d23af7415a0db4f58090
Dani Lyn <dlyn1110@gmail.com>
2020-05-27 20:13:47 +0000

    Automatically backed up by Learn

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -8,6 +8,6 @@ class SightingsController < ApplicationController
 
   def show
     sighting = Sighting.find(params[:id])
-    render json: SightingSerializer.new(sighting)
+    render json: SightingSerializer.new(sighting, options)
   end
 end
